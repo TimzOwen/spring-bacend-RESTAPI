62d72a1457b8c69281797cbfed9c7476d4ac7b15
TimzOwen <timzowen@gmail.com>
2022-09-26 14:09:24 +0300

    mysql connector

--- src/main/java/com/timzowen/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/com/timzowen/springbootbackend/SpringbootBackendApplication.java
@@ -1,14 +1,19 @@
 package com.timzowen.springbootbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class SpringbootBackendApplication {
+@SpringBootApplication
+public class SpringbootBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
