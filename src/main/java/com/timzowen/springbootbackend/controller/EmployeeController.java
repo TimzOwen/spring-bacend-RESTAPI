36c7ac0c3db2a3e398857a5a9c9722c226fc7369
TimzOwen <timzowen@gmail.com>
2022-09-26 10:34:49 +0300

    request mapping

--- src/main/java/com/timzowen/springbootbackend/controller/EmployeeController.java
+++ src/main/java/com/timzowen/springbootbackend/controller/EmployeeController.java
@@ -1,4 +1,24 @@
 package com.timzowen.springbootbackend.controller;
 
+import com.timzowen.springbootbackend.model.Employee;
+import com.timzowen.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
 }
